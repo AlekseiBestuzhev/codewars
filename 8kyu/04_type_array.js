40937353e3d996ca4329d285ca4176f1bd412845
Alexksxx <bestuzhev.aleksei.m@gmail.com>
2022-11-26 00:32:14 +0300

    init: task_04 start

---
+++ 8kyu/04_type_array.js
@@ -0,0 +1,27 @@
+// Task 
+// I've written five functions. Each function receives a parameter arr which is an array. Complete the functions using arr inside the function bodies.
+// Solution
+function getLength(arr) {
+	//return length of arr
+	var arr = [1, 2, 3];
+	return arr.length;
+}
+function getFirst(arr) {
+	//return the first element of arr
+	return ?
+ }
+function getLast(arr) {
+	//return the last element of arr
+	return ?
+ }
+function pushElement(arr) {
+	var el = 1;
+	//push el to arr
+
+	return arr
+}
+function popElement(arr) {
+	//pop an element from arr
+
+	return ?
+ }
