79487e8b7c4f6f450443d5fb888d3923a9dbd017
Alexksxx <bestuzhev.aleksei.m@gmail.com>
2022-12-04 15:01:29 +0300

    update: task_04 complete

--- 8kyu/04_type_array.js
+++ 8kyu/04_type_array.js
@@ -3,25 +3,24 @@
 // Solution
 function getLength(arr) {
 	//return length of arr
-	var arr = [1, 2, 3];
 	return arr.length;
 }
 function getFirst(arr) {
 	//return the first element of arr
-	return ?
- }
+	return arr[0];
+}
 function getLast(arr) {
 	//return the last element of arr
-	return ?
- }
+	return arr[arr.length - 1];
+}
 function pushElement(arr) {
 	var el = 1;
 	//push el to arr
-
-	return arr
+	arr.push(el);
+	return arr;
 }
 function popElement(arr) {
 	//pop an element from arr
-
-	return ?
- }
+	arr.pop();
+	return arr;
+}
