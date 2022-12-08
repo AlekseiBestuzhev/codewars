5dd7345b02fae57e8597db37fb4b9ee9a96ede94
Alexksxx <bestuzhev.aleksei.m@gmail.com>
2022-12-08 23:02:09 +0300

    update: fmm complete

--- 8kyu/find_min_max.js
+++ 8kyu/find_min_max.js
@@ -1,12 +1,23 @@
 // Task 
 // Your task is to make two functions ( max and min, or maximum and minimum, etc., depending on the language ) that receive a list of integers as input, and return the largest and lowest number in that list, respectively.
 // Solution
+let list = [-52, 56, 30, 29, -54, 0, -110];
 var min = function (list) {
-
-	return list[0];
+	let mn = list[0];
+	for (let i = 0; i < list.length; i++) {
+		if (list[i] < mn) {
+			mn = list[i];
+		}
+	}
+	console.log(mn);
 }
 
 var max = function (list) {
-
-	return list[0];
+	let mx = list[0];
+	for (let i = 0; i < list.length; i++) {
+		if (list[i] > mx) {
+			mx = list[i]
+		}
+	}
+	console.log(mx);
 }
