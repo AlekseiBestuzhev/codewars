015ad93396dd46a0b5a7f043ca3d9f0332fd13fd
Alexksxx <bestuzhev.aleksei.m@gmail.com>
2022-12-13 17:23:54 +0300

    update: qom complete

--- 8kyu/quarter_of_month.js
+++ 8kyu/quarter_of_month.js
@@ -3,6 +3,21 @@
 // For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quarter; and month 11 (November), is part of the fourth quarter.
 //Solution
 const quarterOf = (month) => {
-
-
+	if (month > 0 && month <= 3) {
+		return 1;
+	}
+	else if (month > 3 && month <= 6) {
+		return 2;
+	}
+	else if (month > 6 && month <= 9) {
+		return 3;
+	}
+	else if (month > 9 && month <= 12) {
+		return 4;
+	}
+	else {
+		return 'Enter correct number';
+	}
 }
+const a = quarterOf(0);
+console.log(a);
