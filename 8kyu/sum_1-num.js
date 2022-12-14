fa3a15b48b75d72958e83ca3735a4aec2e171011
Alexksxx <bestuzhev.aleksei.m@gmail.com>
2022-12-14 16:51:37 +0300

    update: s1n complete

--- 8kyu/sum_1-num.js
+++ 8kyu/sum_1-num.js
@@ -2,5 +2,11 @@
 // Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
 //Solution
 var summation = function (num) {
-	// Code here
+	let sum = 0;
+	for (let i = 1; i <= num; i++) {
+		sum = sum + i;
+	}
+	return sum;
 }
+const result = summation(3);
+console.log(result);
