cf18f1ddd33ca089989006dfac1d67c762c6056a
Alexksxx <bestuzhev.aleksei.m@gmail.com>
2022-12-26 19:28:15 +0400

    init: rbg start

---
+++ 8kyu/reduce-but-grow.js
@@ -0,0 +1,7 @@
+//Task
+// Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+// [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+//Solution
+function grow(x) {
+
+}
