e22459da76e7e8b8474aad164d75faff54afd31e
Alexksxx <bestuzhev.aleksei.m@gmail.com>
2022-12-26 19:24:55 +0400

    upd: rsb done

--- 8kyu/regular_super_ball.js
+++ 8kyu/regular_super_ball.js
@@ -1,4 +1,11 @@
 //Task
 // Create a class Ball. Ball objects should accept one argument for "ball type" when instantiated. If no arguments are given, ball objects should instantiate with a "ball type" of "regular."
 //Solution
-
+var Ball = function (ballType) {
+	if (!ballType) {
+		this.ballType = 'regular';
+	}
+	else {
+		this.ballType = 'super';
+	}
+};
