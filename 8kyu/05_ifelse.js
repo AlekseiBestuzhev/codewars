45c678cbac1938d88264a6b0b94c1252302d3bb6
Alexksxx <bestuzhev.aleksei.m@gmail.com>
2023-01-08 00:30:15 +0400

    upd: ifelse done

--- 8kyu/05_ifelse.js
+++ 8kyu/05_ifelse.js
@@ -2,5 +2,6 @@
 // Кодирование в функции trueOrFalse, функция принимает 1 параметр: valпопробуйте использовать условный оператор if...else, если valзначение равно false (val==false или оно может быть преобразовано в false), должно вернуть строку "false", если нет, вернуть строку "истина".
 // Solution
 function trueOrFalse(val) {
-
+	if (val) return 'true';
+	else return 'false';
 }
