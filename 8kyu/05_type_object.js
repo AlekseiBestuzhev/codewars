8b881b279f79affaa3e4a10e2283ccaf29853f9c
Alexksxx <bestuzhev.aleksei.m@gmail.com>
2023-01-07 23:47:55 +0400

    init: obj start

---
+++ 8kyu/05_type_object.js
@@ -0,0 +1,9 @@
+// Task 
+// Дайте вам функцию animal, примите 1 параметр: objвот так:
+// {name:"dog",legs:4,color:"white"}
+// и вернуть такую ​​строку:
+// "This white dog has 4 legs."
+// Solution
+function animal(obj) {
+	return
+} 
