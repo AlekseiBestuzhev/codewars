85e08b0c3a1e42ffd4f0b85c329519d55a4c5392
Alexksxx <bestuzhev.aleksei.m@gmail.com>
2023-01-09 16:37:37 +0400

    fix: short name done

--- 8kyu/short-name.js
+++ 8kyu/short-name.js
@@ -7,6 +7,6 @@
 // Solution
 function abbrevName(name) {
 	const arr = name.split(' ');
-	return `${arr[0][0]}.${arr[1][0]}`;
+	return `${arr[0][0].toUpperCase()}.${arr[1][0].toUpperCase()}`;
 }
-console.log(abbrevName("Sam Harris"));
+console.log(abbrevName("sam harris"));
