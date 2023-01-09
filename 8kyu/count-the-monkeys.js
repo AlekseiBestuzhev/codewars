20b61b4ae529f87dfa0fcaa9abbdd74fe4e81472
Alexksxx <bestuzhev.aleksei.m@gmail.com>
2023-01-09 17:01:55 +0400

    upd: count the monkeys done

--- 8kyu/count-the-monkeys.js
+++ 8kyu/count-the-monkeys.js
@@ -6,5 +6,10 @@
 //  1 --> [1]
 // Solution
 function monkeyCount(n) {
-	// your code here
+	const arr = [];
+	for (let i = 1; i <= n; i++) {
+		arr.push(i);
+	}
+	return arr;
 }
+console.log(monkeyCount(11));
