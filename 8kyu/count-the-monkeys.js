795af1b7a42d9374c6bc5bc7ca75fb383ea6a077
Alexksxx <bestuzhev.aleksei.m@gmail.com>
2023-01-09 17:05:17 +0400

    upd: count the monkeys add smarter solutions

--- 8kyu/count-the-monkeys.js
+++ 8kyu/count-the-monkeys.js
@@ -13,3 +13,12 @@ function monkeyCount(n) {
 	return arr;
 }
 console.log(monkeyCount(11));
+
+function monkeyCount(n) {
+	for (var i = 0, arr = []; i < n; arr.push(++i));
+	return arr;
+}
+
+function monkeyCount(n) {
+	return Array.from({ length: n }, (_, i) => i + 1)
+}
