a6a11a1b7f7b09a2a9c5e8a8d1685b1bc7b9534d
Alexksxx <bestuzhev.aleksei.m@gmail.com>
2023-01-09 17:38:14 +0400

    init: itsu start

---
+++ 8kyu/is-the string-uppercase.js
@@ -0,0 +1,9 @@
+// Task 
+// Examples (input -> output)
+// "c" -> False
+// "C" -> True
+// "hello I AM DONALD" -> False
+// "HELLO I AM DONALD" -> True
+// "ACSKLDFJSgSKLDFJSKLDFJ" -> False
+// "ACSKLDFJSGSKLDFJSKLDFJ" -> True
+// Solution
