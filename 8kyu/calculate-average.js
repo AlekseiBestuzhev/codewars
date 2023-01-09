cf043dc45dd6c8be2fb94672054bbaf721c2ef60
Alexksxx <bestuzhev.aleksei.m@gmail.com>
2023-01-09 22:10:13 +0400

    upd: cal aver done

--- 8kyu/calculate-average.js
+++ 8kyu/calculate-average.js
@@ -3,5 +3,9 @@
 // Note: Empty arrays should return 0.
 // Solution
 function findAverage(array) {
-
+	let result = 0;
+	for (let i = 0; i < array.length; i++) {
+		result += array[i];
+	}
+	return result / array.length || 0;
 }
