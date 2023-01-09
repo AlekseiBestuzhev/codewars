f28fe10baceb31f9b7479789751d44048efc50b4
Alexksxx <bestuzhev.aleksei.m@gmail.com>
2023-01-09 16:51:13 +0400

    upd: itaw done

--- 8kyu/is-there-a-wowel.js
+++ 8kyu/is-there-a-wowel.js
@@ -3,3 +3,10 @@
 // Если это так, измените значение массива на строку этой гласной.
 // Вернуть полученный массив.
 // Solution
+function isVow(a) {
+	return a.map(
+		charCode =>
+			/[aeiou]/.test(String.fromCharCode(charCode))
+				? String.fromCharCode(charCode)
+				: charCode)
+}
